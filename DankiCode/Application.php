38cf7eedcb8438409aea013b967322b5c311070e
Victor Valente <Valente-victor@hotmail.com>
2023-08-17 19:48:10 -0300

    Carregamento de paginas com arquitetura MVC

--- DankiCode/Application.php
+++ DankiCode/Application.php
@@ -3,8 +3,33 @@
 
     class Application{
 
+        private $controller;
+
+        private function setApp(){
+
+            $loadName = 'DankiCode\Controllers\\';
+            $url = explode('/', @$_GET['url']);
+
+            if (!isset($url[0]) || $url[0] == '') {
+                $loadName .= 'Home';
+            } else {
+                $loadName .= ucfirst(strtolower($url[0]));
+            }
+
+            $loadName.='Controller';
+
+            if(file_exists($loadName.'.php')){
+                $this->controller = new $loadName();
+            }else{
+                include('Views/pages/404.php');
+                die();
+            }
+
+        }
+
         public function run(){
-            echo 'rodadando';
+            $this->setApp();
+            $this->controller->index(); 
         }
     }
 ?>
---
+++ DankiCode/Views/pages/404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Erro</title>
+</head>
+<body>
+    <h2>Erro 404 -> essa pagnia nao existe</h2>
+    
+</body>
+</html>
